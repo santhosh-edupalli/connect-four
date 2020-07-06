bea8381b340a1d79eb3e2bf8c93dd8a64412e4ed
santhosh edupalli <evssanthosh@gmail.com>
2020-07-06 17:53:47 +0530

    build files added

---
+++ build/precache-manifest.b0f17e671d6206d6e32163b0ebbca2dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e3c45847933d4d2d74cf51d4ef2bc9e",
+    "url": "/connect-four/index.html"
+  },
+  {
+    "revision": "96ac1c239b470c50859d",
+    "url": "/connect-four/static/css/main.cf5684f1.chunk.css"
+  },
+  {
+    "revision": "098925d9bb745575c3ed",
+    "url": "/connect-four/static/js/2.42f5908e.chunk.js"
+  },
+  {
+    "revision": "c9dd85361cb9cef9841c7e9bd1147de5",
+    "url": "/connect-four/static/js/2.42f5908e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96ac1c239b470c50859d",
+    "url": "/connect-four/static/js/main.781d929e.chunk.js"
+  },
+  {
+    "revision": "5df7cfc0111087909443",
+    "url": "/connect-four/static/js/runtime-main.347ee35c.js"
+  },
+  {
+    "revision": "d10b9ad2f219f337692b94d38fe6f701",
+    "url": "/connect-four/static/media/4inarow.d10b9ad2.png"
+  }
+]);
